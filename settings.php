8dd77fecdb27eb3400796195f11973f5d1c014e8
socratous139 <socratous139@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-07 11:10:50 +0000

    Fixed registration user/email duplication
    git-svn-id: https://plugins.svn.wordpress.org/vooplayer/tags/1.0@709168 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- settings.php
+++ settings.php
@@ -31,10 +31,10 @@ if (isset($_POST['act']) && $_POST['act']=="logout")
 					<input type="hidden" name="returnurl" value="<?php echo admin_url("admin-ajax.php?action=voo_call");?>">
 					<table>
 						<tr>
-							<td><input type="text" name="amember_login" size="15" placeholder='<?php _e('Username', $this->namespace); ?>'></td>
+							<td><input type="text" name="amember_login" id="login_amember_login" size="15" placeholder='<?php _e('Vooplayer.com Username', $this->namespace); ?>' class="voo_input"></td>
 						</tr>
 						<tr>
-							<td><input type="password" name="amember_pass" size="15" placeholder='<?php _e('Password', $this->namespace); ?>'></td>
+							<td><input type="password" name="amember_pass" id="login_amember_pass" size="15" placeholder='<?php _e('Vooplayer.com Password', $this->namespace); ?>' class="voo_input"></td>
 						</tr>
 						<tr>
 							<td><a href="http://www.vooplayer.com/members/login.php"><?php _e('Forgot Password', $this->namespace); ?>?</a></td>
@@ -60,11 +60,11 @@ if (isset($_POST['act']) && $_POST['act']=="logout")
 				
 				<table>
 						<tr>
-							<td><input type="text" name="amember_login" size="15" id="amember_login" placeholder="<?php _e('Username', $this->namespace); ?>"></td>
-							<td><input type="password" name="amember_pass" size="15" placeholder="<?php _e('Password', $this->namespace); ?>"></td>
+							<td><input type="text" name="amember_login" size="15" id="reg_amember_login" placeholder="<?php _e('Choose Username', $this->namespace); ?>" class="voo_input"></td>
+							<td><input type="password" name="amember_pass" id="reg_amember_pass"size="15" placeholder="<?php _e('Choose Password', $this->namespace); ?>" class="voo_input"></td>
 						</tr>
 						<tr>
-							<td colspan="3"><input type="text" name="email" size="30" id="email" placeholder="<?php _e('Email Address', $this->namespace); ?>"></td>
+							<td colspan="3"><input type="text" name="email" size="30" id="reg_email" placeholder="<?php _e('Email Address', $this->namespace); ?>" class="voo_input"></td>
 						</tr>
 						<tr>
 							<td>&nbsp;</td>
@@ -80,87 +80,6 @@ if (isset($_POST['act']) && $_POST['act']=="logout")
 			<?php }?>
 			<div style="clear:both;float:none;"></div>
 		</div>
-		<!-- ------>
-		<!-- <div class="postbox">
-			<h3 class="hndle"><?php _e('Existing Vooplayer User', $this->namespace); ?></h3>
-			<div class="inside col">
-				<h4 class="coltitle"><?php _e('Login', $this->namespace); ?></h4>
-				
-				<form id="vooplayer_login" name="vooplayer_login" method="post" class="apicall">
-					<input type="hidden" name="act" value="login" class="act">
-					<input type="hidden" name="returnurl" value="<?php echo admin_url("admin-ajax.php?action=voo_call");?>">
-					<table>
-						<tr>
-							<td><?php _e('Username', $this->namespace); ?></td>
-							<td><input type="text" name="amember_login" size="15"></td>
-						</tr>
-						<tr>
-							<td><?php _e('Password', $this->namespace); ?></td>
-							<td><input type="password" name="amember_pass" size="15"></td>
-						</tr>
-						<tr>
-							<td><input type="submit" name="login"  value="<?php _e('Login', $this->namespace); ?>" class="button-primary">&nbsp;<img src='<?php echo $this->plugin_url?>/images/loading.gif' id='login_wait' class="wait" style='display:none;'></td>
-							
-						</tr>
-					</table>
-					<div id="login_msg" style="height:35px;color:#00000; margin-top:10px;" class="msg"></div>
-				</form>
-			</div>
-			<div class="inside col">
-				<h4 class="coltitle"><?php _e('Lost Password', $this->namespace); ?>?</h4>
-				<form id="vooplayer_forgot" name="vooplayer_forgot" method="post" action="http://www.vooplayer.com/members/login.php" target="_new">
-					<table>
-
-						<tr>
-							<td><input type="submit" name="forgot" value="<?php _e('Get Password', $this->namespace); ?>" class="button-primary"></td>
-						</tr>
-					</table>
-				</form>
-			</div>
-			<div class="inside col">
-				<h4 class="coltitle"><?php _e('Help', $this->namespace); ?></h4>
-				<?php _e('You must have registered with Vooplayer.', $this->namespace); ?><br/>
-				<?php _e('Username, password and email address asked on', $this->namespace); ?> <br/><?php _e('this screen are related to your Vooplayer account.', $this->namespace); ?> <br/><br/>
-				<?php _e('Use the Help tab in the upper right of screen for more details.', $this->namespace); ?>
-			</div>
-			<div style="clear:both;float:none;"></div>
-		</div>
-		<div class="postbox">
-			<h3 class="hndle"><?php _e('New User', $this->namespace); ?></h3>
-			<div class="inside col">
-			<h4 class="coltitle"><?php _e('Register', $this->namespace); ?></h4>
-           
-
-			<form id="vooplayer_register" name="vooplayer_register" method="post" class="apicall">
-				<input type="hidden" name="act" value="reg" class="act">
-				<input type="hidden" name="returnurl" value="<?php echo admin_url("admin-ajax.php?action=voo_call");?>">
-				<table>
-						<tr>
-							<td><?php _e('Username', $this->namespace); ?></td>
-							<td><input type="text" name="amember_login" size="15" id="amember_login"></td>
-							<td><?php _e('Password', $this->namespace); ?></td>
-							<td><input type="password" name="amember_pass" size="15"></td>
-						</tr>
-						<tr>
-							<td><?php _e('Email Address', $this->namespace); ?></td>
-							<td colspan="3"><input type="text" name="email" size="30" id="email"></td>
-						</tr>
-						<tr>
-							<td><input type="submit" name="register"  value="<?php _e('Register', $this->namespace); ?>" class="button-primary">&nbsp;<img src='<?php echo $this->plugin_url?>/images/loading.gif' id='reg_wait' class="wait" style='display:none;'></td>
-							
-						</tr>
-					</table>
-					<div id="reg_msg" style="height:35px;color:#00000; margin-top:10px;" class="msg"></div>
-			</form>
-			</div>
-			<div class="inside col">
-				<h4 class="coltitle"><?php _e('Help', $this->namespace); ?></h4>
-				<?php _e('This screen will create your Vooplayer Free account.', $this->namespace); ?><br/>
-				<br/>
-				<?php _e('Use the Help tab in the upper right of screen for more details.', $this->namespace); ?>
-			</div>
-			<div style="clear:both;float:none;"></div>
-		</div> -->
 	</div>
 </div>
 <form method="post" id="frmlogout">
@@ -180,22 +99,38 @@ if (isset($_POST['act']) && $_POST['act']=="logout")
 		if (act == "reg")
 		{
 			actname = "Registration";
-			jQuery("#frmaweber").attr('target','vooCall');
-			jQuery("#aweber_email").val(jQuery("#email").val());
-			jQuery("#aweber_amember_login").val(jQuery("#amember_login").val());
-			jQuery("#frmaweber").attr('action','<?php echo $this->service_url;?>members/webservice_aweber.php');
-			jQuery("#frmaweber").submit();
 		}
 		jQuery('.msg').hide();
 		jQuery("#"+act+"_msg").show();
-		if (valid!=0)
+		if (valid>0)
 		{
 			jQuery("#"+act+"_msg").html(actname + " Successfull.");
+			
+			if (act == "reg")
+			{
+				actname = "Registration";
+				jQuery("#frmaweber").attr('target','vooCall');
+				jQuery("#aweber_email").val(jQuery("#email").val());
+				jQuery("#aweber_amember_login").val(jQuery("#amember_login").val());
+				jQuery("#frmaweber").attr('action','<?php echo $this->service_url;?>members/webservice_aweber.php');
+				//jQuery("#frmaweber").submit();
+			}
 			var t=setTimeout(function(){location.href = '<?php echo admin_url("admin.php?page=vooplayer")?>';},1000);
 		}
 		else
 		{
-			jQuery("#"+act+"_msg").html(actname + " failed. Try again.");
+			if (valid == -1)
+			{
+				jQuery("#"+act+"_msg").html("user name already exists. Try again.");
+			}
+			else if (valid == -2)
+			{
+				jQuery("#"+act+"_msg").html("email id already exists. Try again.");
+			}
+			else
+			{
+				jQuery("#"+act+"_msg").html(actname + " failed. Try again.");
+			}
 		}
 		jQuery('#apiid').remove();
 	}
@@ -206,14 +141,38 @@ if (isset($_POST['act']) && $_POST['act']=="logout")
 		});
 		jQuery('<i'+'fra'+'me id="vooCall" name="vooCall" frameborder="0" width="1px" height="1px">').appendTo('body');
 		jQuery(".apicall").submit(function(){
+			var act = jQuery(this).find(".act").val();
+			var error = false;
+			if (jQuery("#"+act+"_amember_login").length > 0 && jQuery.trim(jQuery("#"+act+"_amember_login").val())== "")
+			{
+				error = true;
+				jQuery("#"+act+"_amember_login").css('background','#FFC1B9');
+			}
+			if (jQuery("#"+act+"_amember_pass").length > 0 && jQuery.trim(jQuery("#"+act+"_amember_pass").val())== "")
+			{
+				error = true;
+				jQuery("#"+act+"_amember_pass").css('background','#FFC1B9');
+			}
+			if (jQuery("#"+act+"_email").length > 0 && jQuery.trim(jQuery("#"+act+"_email").val())== "")
+			{
+				error = true;
+				jQuery("#"+act+"_email").css('background','#FFC1B9');
+			}
+			if (error)
+			{
+				return false;
+			}
 			jQuery('#apiid').remove();
 			jQuery('<input type="hidden" name="ap'+'ii'+'d" id="ap'+'ii'+'d" value="v'+'o'+'o_'+'wp">').appendTo(this);
 			jQuery(this).attr('target','vooCall');
 			jQuery(this).attr('action','<?php echo $this->service_url;?>members/webservice_member_acc.php');
-			var act = jQuery(this).find(".act").val();
+
 			jQuery('.wait').hide();
 			jQuery("#"+act+"_wait").show();
 			return true;
+		});
+		jQuery('.voo_input').focus(function(){
+			jQuery(this).css('background','#ffffff');
 		});
 	});
 </script>
